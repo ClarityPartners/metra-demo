a2e4fdac0d24adb1be61099176f56d870c035ed7
D. Elion <d.elion@claritypartners.com>
2019-10-24 10:29:26 -0500

    changing the order of slide items

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ jQuery(window).on('load',function(){
 	  slidesToScroll: 1,
 	  autoplay: false,
 	  centerMode: true,
-	  //asNavFor: '.box_slide',
+	  asNavFor: '.box_slide',
 	  infinite: true,
 	  prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
       nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
